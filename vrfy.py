d1dc3bb80833c76362acf30583cd9e7c6fd364f7
SezioS <118485549+sergiovks@users.noreply.github.com>
2023-02-28 18:34:22 +0100

    Update vrfy.py

--- vrfy.py
+++ vrfy.py
@@ -1,22 +1,29 @@
 import socket
 import sys
 
-if len(sys.argv) != 2:
-  print("Usage: vrfy.py USERNAME")
+if len(sys.argv) == 2 and sys.argv[1] == '-h':
+  print("Usage: vrfy.py -ip SMTP_IP -w WORDLIST_FILE")
+  sys.exit(0)
+
+if len(sys.argv) != 4 or sys.argv[1] != '-ip':
+  print("Usage: vrfy.py -ip SMTP_IP -w WORDLIST_FILE")
   sys.exit(0)
   
 #Create a Socket and connect to the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connect = s.connect(('10.11.1.217',25))
+connect = s.connect((sys.argv[2],25))
 
 #Receive the banner
 banner = s.recv(1024)
 print(banner)
 
-#VRFY a user
-s.send('VRFY' + sys.argv[1] + '\r\n')
-result = s.recv(1024)
-print(result)
+#Read usernames from wordlist file
+with open(sys.argv[3], 'r') as f:
+    for line in f:
+        #VRFY each username
+        s.send('VRFY ' + line.strip() + '\r\n')
+        result = s.recv(1024)
+        print(result)
 
 #Close the socket
 s.close()
