d9651042aeef691d8a214d0ca266094487b7aa29
SezioS <118485549+sergiovks@users.noreply.github.com>
2023-03-04 18:13:02 +0100

    Update vrfy.py

--- vrfy.py
+++ vrfy.py
@@ -9,31 +9,31 @@ def banners():
     *********************************************
     """)
 
-banners
+banners()
 
 if len(sys.argv) == 2 and sys.argv[1] == '-h':
-  print("Usage: vrfy.py -ip SMTP_IP -w WORDLIST_FILE")
-  sys.exit(0)
+    print("Usage: vrfy.py -ip SMTP_IP -w WORDLIST_FILE")
+    sys.exit(0)
 
 if len(sys.argv) != 4 or sys.argv[1] != '-ip':
-  print("Usage: vrfy.py -ip SMTP_IP -w WORDLIST_FILE")
-  sys.exit(0)
-  
-#Create a Socket and connect to the server
+    print("Usage: vrfy.py -ip SMTP_IP -w WORDLIST_FILE")
+    sys.exit(0)
+
+# Create a Socket and connect to the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connect = s.connect((sys.argv[2],25))
+connect = s.connect((sys.argv[2], 25))
 
-#Receive the banner
+# Receive the banner
 banner = s.recv(1024)
-print(banner)
+print(banner.decode())
 
-#Read usernames from wordlist file
+# Read usernames from wordlist file
 with open(sys.argv[3], 'r') as f:
     for line in f:
-        #VRFY each username
-        s.send('VRFY ' + line.strip() + '\r\n')
+        # VRFY each username
+        s.send(('VRFY ' + line.strip() + '\r\n').encode())
         result = s.recv(1024)
-        print(result)
+        print(result.decode())
 
-#Close the socket
+# Close the socket
 s.close()
